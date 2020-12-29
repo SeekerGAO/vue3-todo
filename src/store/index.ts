1fa31664fb52a8f3d10dcf5e7b5fab68c059c59d
SeekerGAO <1532584997@qq.com>
2020-12-29 16:38:12 +0800

    feat: Add store

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,16 +4,42 @@ import createPersistedState from "vuex-persistedstate";
 export default createStore({
   plugins: [createPersistedState()],
   state: {
-    todos: [] as Array<string>,
+    todos: [] as Array<Object>,
+    visibility: "all",
+    uid: 0,
   },
   mutations: {
-    updateTodos(state, todos) {
+    UPDATE_TODOS(state, todos) {
       state.todos = todos;
+    },
+    ADD_TODO(state, todo) {
+      state.todos.push({
+        id: state.uid,
+        title: todo,
+        completed: false,
+      });
+
+      state.uid++;
+    },
+    REMOVE_TODO(state, todo) {
+      state.todos.splice(state.todos.indexOf(todo), 1);
+    },
+    SET_VISIBILITY(state, type) {
+      state.visibility = type;
     },
   },
   actions: {
     updateTodos({ commit }, todos) {
-      commit("updateTodos", todos);
+      commit("UPDATE_TODOS", todos);
+    },
+    addTodo({ commit }, todo) {
+      commit("ADD_TODO", todo);
+    },
+    removeTodo({ commit }, todo) {
+      commit("REMOVE_TODO", todo);
+    },
+    setVisibility({ commit }, type) {
+      commit("SET_VISIBILITY", type);
     },
   },
   modules: {},
